287e4aa3465aad1e79b71931a3ddead43eefb3c5
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-26 14:28:01 -0700

    add repeatedWordCounter function

--- README.md
+++ README.md
@@ -20,4 +20,44 @@ Test: "It should return 0 for a string that is only spaces."
 Code: wordCounter("            ");
 Expected Output: 0
 
-Co-authored-by: Spencer Watari<deanwatari@gmail.com> and Justin Shaffer<justinbshaffer91@gmail.com>
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
+
+Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code:
+const text = "";
+const word = "";
+numberOfOccurrencesInText(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo</p>
+
+Co-authored-by: Spencer Watari<deanwatari@gmail.com>, Justin Shaffer<justinbshaffer91@gmail.com>, <bryce.bresnan@gmail.com>
+
+
+Describe: repeatedWordCounter()
+
+Test: "It should return 1 if a word is repeated."
+Code:
+
+const text = "bing bing";
+repeatedWordCounter(text);
+Expected Output = 1
+
+Test: "It should return 0 if a word is not repeated."
+Code:
+const text = "bing pong";
+repeatedWordCounter(text);
+Expected Output = 0
+
--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,61 @@ function wordCounter(text) {
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
     wordCount++;
+    }
   });
   return wordCount;
 }
+
+function numberOfOccurrencesInText(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+    p.append(text);
+    return p;
+}
+
+function repeatedWordCounter(text) {
+  const textArray = text.split(" ");
+  const repeatArray = [textArray[0]];
+  textArray.forEach(function(element) {
+    repeatArray.forEach(function(repeatElement) {
+      if (element === repeatElement) {
+        return 1;
+      }
+    });
+  });
+}
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
